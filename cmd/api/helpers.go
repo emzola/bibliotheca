9bdc24084c387afce8c3fddd1d267c49f2f0f29c
emzola <emma.idika@yahoo.com>
2023-04-30 17:31:25 +0300

    Create HTTP server and healthcheck handler

---
+++ cmd/api/helpers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type envelope map[string]interface{}
+
+func (app *application) encodeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	for k, v := range headers {
+		w.Header()[k] = v
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"os"
+
+	"github.com/emzola/bibliotheca/internal/jsonlog"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *jsonlog.Logger
+}
 
 func main() {
-	fmt.Println("Hello Bibliotheca!")
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.Parse()
+
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err := app.serve()
+	if err != nil {
+		app.logger.PrintFatal(err, nil)
+	}
 }
